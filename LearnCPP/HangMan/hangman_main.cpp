ea7a958622421e531ce0346f01df44df485c3fe7
cveney <christina.veney11@gmail.com>
2020-09-24 23:05:36 -0600

    start button functioning and working to create gameplay screen

--- LearnCPP/HangMan/hangman_main.cpp
+++ LearnCPP/HangMan/hangman_main.cpp
@@ -1,3 +1,6 @@
+#include <SFML/System/String.hpp>
+#include <SFML/Graphics/Export.hpp>
+#include <SFML/Graphics/Glyph.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
@@ -8,7 +11,8 @@ using namespace std;
 /*Primary goal with this program is to attempt to code a game of hangman from scratch without looking at a specific tutorial but instead
  * working from psuedo code to attempt to create the game and referencing small tutorials and lessons as I go to accomplish to overall game*/
 
-/*CURRENT STATUS: need to create function call for game now that start button works and apply texture to actual start button*/
+/*CURRENT STATUS: need to figure out how to get text to display in the new cleared version of the window after start button is pressed and
+and create a game over sprite or text*/
 
 //Function to see if mouse is over a button
 bool isHover(sf::FloatRect button, sf::Vector2f mouse)
@@ -25,25 +29,30 @@ int main()
     //initialize variables used throughout gameplay
     int win_height = 600;
     int win_width = 1000;
+    bool isPlaying = false;
+    bool gameLost = false;
+    sf::Font font;
+
+    //Load and check the availability of the font file
+    if(!font.loadFromFile("fonts/arial.ttf"))
+    {
+        cout << "can't load font" << endl;
+    }
 
     //create game window with label
     sf::RenderWindow window(sf::VideoMode(win_width,win_height), "Let's Play Hangman");
     sf::Vector2i mousePos = sf::Mouse::getPosition(window);
 
     //generate start button
-    sf::RectangleShape startButton(sf::Vector2f(80,50));
-    startButton.setFillColor(sf::Color::Green);
-    startButton.setPosition(win_width/2, win_height/2);
+    sf::CircleShape startButton;
+    startButton.setRadius(50);
+    startButton.setPosition((win_width/2)-25, (win_height/2)-25);
     float startButtonWidth = startButton.getLocalBounds().width;
     float startButtonHeight = startButton.getLocalBounds().height;
-
     sf::Texture texture;
     if (!texture.loadFromFile(("start-button.png")))
         return -1;
-    // Assign it to a sprite
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
-    sprite.scale(sf::Vector2f(.15f, .15f));
+    startButton.setTexture(&texture);
 
 
     while (window.isOpen())
@@ -62,17 +71,33 @@ int main()
                 if(event.type == sf::Event::MouseButtonReleased &&  event.mouseButton.button == sf::Mouse::Left)
                 {
                     cout <<"confirmation"<<endl;
+                    isPlaying = true;
                 }
             }
+            if(isPlaying)
+            {
+                window.clear(sf::Color::Blue);
+                window.display();
+
+                sf::Text text;
+                text.setString("Hello world");
+                text.setCharacterSize(30);
+                text.setFillColor(sf::Color::Red);
+                text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+                window.draw(text);
+            }
+            if(!isPlaying)
+            {
+                window.clear();
+                if(!gameLost)
+                {
+                    window.draw(startButton);
+                }else{
+                    //window.draw(gameOver);
+                }
+                window.display();
+            }
         }
-
-        window.clear();
-        window.draw(startButton);
-        window.draw(sprite);
-        window.display();
     }
-
-
-
     return 0;
 }
